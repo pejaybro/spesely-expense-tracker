cf848ad2ff56270f75faf9d22c71fd3fa50ada20
Prayas Jadli <prayasjadli18@gmail.com>
2026-03-08 00:52:58 +0530

    inital setup

---
+++ apps/desktop/electron/main.cjs
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+const isDev = process.env.NODE_ENV === "development";
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.cjs"),
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// IPC handlers
+ipcMain.handle("ping", () => "pong");
